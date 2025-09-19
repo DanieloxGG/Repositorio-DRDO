27588bb4e5cde20263f3b6a672e47950774e3462
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-19 15:55:43 +0200

    Create Ejercicio11.py

---
+++ EJERCICIOS INICIALES/Ejercicio11.py
@@ -0,0 +1,8 @@
+# 11: Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva 
+# por pantalla en el área y el perímetro
+
+lado=float(input("Introduce el lado (cm) del cuadrado: "))
+print("")
+
+print(f"El perímetro del cuadrado es de {lado*4} centímetros.")
+print(f"El área del cuadrado es de {lado**2} centímetros cuadrados.")
