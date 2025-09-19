42aa47a3c63620cbb9b5edd663d62520eff1a06f
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-19 15:56:06 +0200

    Create Ejercicio5.py

---
+++ EJERCICIOS INICIALES/Ejercicio5.py
@@ -0,0 +1,11 @@
+# 5:  Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código 
+# para que entre palabra y palabra haya una coma.
+
+pal1=input("Introduce la palabra 1: ")
+pal2=input("Introduce la palabra 2: ")
+pal3=input("Introduce la palabra 3: ")
+pal4=input("Introduce la palabra 4: ")
+pal5=input("Introduce la palabra 5: ")
+print("")
+
+print(f"{pal1}, {pal2}, {pal3}, {pal4}, {pal5}.")
