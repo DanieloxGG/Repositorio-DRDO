808bb49918689a7191e7ca58887b6aabd9134afc
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-19 15:56:11 +0200

    Create Ejercicio6.py

---
+++ EJERCICIOS INICIALES/Ejercicio6.py
@@ -0,0 +1,12 @@
+# 6:  A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden 
+# inverso en que se han introducido las palabras.
+
+pal1=input("Introduce la palabra 1: ")
+pal2=input("Introduce la palabra 2: ")
+pal3=input("Introduce la palabra 3: ")
+pal4=input("Introduce la palabra 4: ")
+pal5=input("Introduce la palabra 5: ")
+print("")
+
+print(f"{pal1} {pal2} {pal3} {pal4} {pal5}.")
+print(f"{pal5} {pal4} {pal3} {pal2} {pal1}.")
