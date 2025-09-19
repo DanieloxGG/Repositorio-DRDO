88b0918d6c8957e7ebec05628f3d6831e3539ae6
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-19 15:55:55 +0200

    Create Ejercicio2.py

---
+++ EJERCICIOS INICIALES/Ejercicio2.py
@@ -0,0 +1,12 @@
+# 2:  Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla
+# en el siguiente orden: número entero, texto y número decimal.
+
+var1=input("Escribe un número entero: ")
+var2=input("Escribe un valor textual: ")
+var3=float(input("Escribe un valor decimal: "))
+print("")
+
+print(f"El número entero es {var1}.")
+print(f"El valor textual es {var2}.")
+print(f"El valor decimal es {var3}.")
+print("")
