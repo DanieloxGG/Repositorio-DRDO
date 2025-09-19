6a1b9b9426047901bf21200217b7927e8e3fbd3b
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-19 15:56:02 +0200

    Create Ejercicio4.py

---
+++ EJERCICIOS INICIALES/Ejercicio4.py
@@ -0,0 +1,8 @@
+# 4:  A partir del código anterior, realiza una versión para números con decimales
+
+var1=float(input("Escribe un número decimal: "))
+var2=float(input("Escribe otro número decimal: "))
+print("")
+
+print(f"La suma de {var1} y {var2} es igual a {var1+var2}.")
+print("")
