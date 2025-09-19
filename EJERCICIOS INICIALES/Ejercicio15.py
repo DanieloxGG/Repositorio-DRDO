bb4715c7afdb31bb6af2209389bf84dc76aba4b0
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-19 16:04:56 +0200

    Create Ejercicio15.py

---
+++ EJERCICIOS INICIALES/Ejercicio15.py
@@ -0,0 +1,8 @@
+# 15: Utiliza el valor Pi de la librería math para calcular el área y volumen de un cilindro, 
+# introduciendo por teclado el valor de radio y altura. Resultado con 2 decimales.
+
+import math
+rad=float(input("Escribe el radio del cilindro: "))
+alt=float(input("Escribe la altura del cilindro: "))
+
+area=math.pi
