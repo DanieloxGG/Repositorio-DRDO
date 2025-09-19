0abd58904660cb38ed0873e8a9cafce81add9734
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-19 15:55:57 +0200

    Create Ejercicio3.py

---
+++ EJERCICIOS INICIALES/Ejercicio3.py
@@ -0,0 +1,8 @@
+# 3:  Programa que pida dos números enteros y realice la suma correspondiente.
+
+var1=(input("Escribe un número: "))
+var2=(input("Escribe otro número: "))
+print("")
+
+print(f"La suma de {var1} y {var2} es igual a {var1+var2}.")
+print("")
