68f5fe3fe76a7124e1e51cef7fd1a50b5377f640
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-23 10:46:57 +0200

    Update actividad1guiada.py

--- EJERCICIOS INICIALES/actividad1guiada.py
+++ EJERCICIOS INICIALES/actividad1guiada.py
@@ -1,5 +1,8 @@
+# Programa que realiza la suma de dos números.
+
 var1=float(input("Introduce un número: "))
 var2=float(input("Introduce otro número: "))
 
 total=var1+var2
+
 print(f"La suma de los números es {var1+var2}.")
