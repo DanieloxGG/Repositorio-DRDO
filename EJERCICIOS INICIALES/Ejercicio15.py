08de42b423e75a6dd2f86ff358321209c4bf5bc2
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-09-23 10:40:52 +0200

    Update Ejercicio15.py

--- EJERCICIOS INICIALES/Ejercicio15.py
+++ EJERCICIOS INICIALES/Ejercicio15.py
@@ -4,5 +4,10 @@
 import math
 rad=float(input("Escribe el radio del cilindro: "))
 alt=float(input("Escribe la altura del cilindro: "))
+print("")
 
-area=math.pi
+area=round(2*math.pi*rad*(rad+alt), 2)
+vol=round(math.pi*rad**2*alt, 2)
+
+print(f"El área del cilindro es {area}.")
+print(f"El volumen del cilindro es {vol}.")
