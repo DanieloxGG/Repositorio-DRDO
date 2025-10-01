90d5a0f9e19efe056f4a22e07424eee2c08231a5
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-10-01 09:56:13 +0200

    Update Ejercicio21.py

--- EJERCICIOS CONDICIONALES/Ejercicio21.py
+++ EJERCICIOS CONDICIONALES/Ejercicio21.py
@@ -7,12 +7,13 @@ num1=float(input("Introduce el valor del primer número: "))
 num2=float(input("Introduce el valor del segundo número: "))
 num3=float(input("Introduce el valor del tercer número: "))
 num=float(num2**2-4*num1*num3)
+print("")
 
-if math.sqrt(num) > 0:
-    resultadopos=(num2*-1+math.sqrt(num2**2-4*num1*num3))/2*num1
-    resultadoneg=(num2*-1-math.sqrt(num2**2-4*num1*num3))/2*num1
+if num > 0:
+    resultadopos=(num2*-1+math.sqrt(num))/2*num1
+    resultadoneg=(num2*-1-math.sqrt(num))/2*num1
 
-    print(f"El valor de x1 es {resultadopos}.")
-    print(f"El valor de x2 es {resultadoneg}.")
+    print(f"El valor de x1 es {round(resultadopos, 2)}.")
+    print(f"El valor de x2 es {round(resultadoneg, 2)}.")
 else:
     print("Error, el valor de la raíz cuadrada es negativo.")
