0a9fae09da4240e7e352f5a0c2cc4996cfbf3bf9
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-10-03 15:58:47 +0200

    Create Ejercicio32.py

---
+++ EJERCICIOS CONDICIONALES/Ejercicio32.py
@@ -0,0 +1,15 @@
+# 32. ¿Cómo solucionarías con ayuda de métodos String el problema del ejercicio anterior para 
+# no distinguir entre mayúsculas y minúsculas?
+
+print("")
+print("La frase es: A quien madruga Dios ayuda.")
+frase="A quien madruga Dios ayuda."
+frase=frase.lower()
+palabra=input("Introduce una palabra, que esté en la frase o que no esté: ")
+palabra=palabra.lower()
+print("")
+
+if frase.__contains__(palabra):
+    print(f"La palabra {palabra} está en la frase, en el índice {frase.index(palabra)}.")
+else:
+    print(f"La palabra {palabra} no está en la frase.")
