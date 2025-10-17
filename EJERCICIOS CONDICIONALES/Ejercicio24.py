2d941b0fa6edd042674b974a0df3cd3bcfa4c238
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-10-17 16:05:59 +0200

    Update Ejercicio24.py

--- EJERCICIOS CONDICIONALES/Ejercicio24.py
+++ EJERCICIOS CONDICIONALES/Ejercicio24.py
@@ -1,13 +1,15 @@
 # 24: Corrige los errores del siguiente código y comprueba que se ejecuta correctamente.1var=float(input("Introduce el peso: "))
 
 # En vez de nombrar a las variables como "1var" y "2var" es mejor nombrarlas "var1" y var2".
-var1=float(input("Introduce el peso: "))
+# Tambien vendría bien concretarle al usuario que unidades se usan para evitar confusión.
+var1=float(input("Introduce el peso (Kg): "))
 # Aquí falta el "float()".
-var2=float((input("Introduce la altura: ")))
+var2=float((input("Introduce la altura (m): ")))
 # Aquí tendríamos que poner solo un "="
 var_imc=var1/var2**2
 # Este "print" necesita una "f" al principio, y la variable 1 está en mayúsculas.
-print(f"Si pesas {var1} kilos y mides {var2}, tu IMC es: {var_imc}.")
+# También sería conveniente poner un "round()" para que el resultado quede más bonito.
+print(f"Si pesas {var1} kilos y mides {var2}, tu IMC es: {round(var_imc, 2)}.")
 # Faltan los ":" al final del condicional.
 if var_imc>25:
  print("Hay sobrepeso")
