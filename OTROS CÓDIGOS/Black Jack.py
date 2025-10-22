40a96d9ee8a1e1f8bf88b8f823e2341c9ce9d4f4
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-10-22 09:55:48 +0200

    Update Black Jack.py

--- OTROS CÓDIGOS/Black Jack.py
+++ OTROS CÓDIGOS/Black Jack.py
@@ -18,19 +18,38 @@ print("")
 juego=1
 
 while juego==1:
+    numjug=0
+    numcrupier=0
     elegir=1
-    dinero=dinero-2
-    print(f"Tienes {dinero} monedas.")
+    print(f"Tienes {dinero} monedas antes de apostar.")
+    sleep(1)
+    apostar=1
+    while apostar == 1:
+        apuesta=float(input("¿Cuantas monedas quieres apostar?: "))
+        if round(apuesta) == apuesta:
+            if apuesta == dinero or apuesta < dinero:
+                print("")
+                apostar=0
+                sleep(1)
+            else:
+                print("No puedes apostar dinero que no tienes.")
+                print("")
+        else:
+            print(f"{apuesta} no es una apuesta válida.")
+            print("")
+    print(f"Tanto tú como el crupier apostais {round(apuesta)} monedas.")
+    dinero=dinero-round(apuesta)
+    sleep(1)
+    print(f"Te quedan {dinero} monedas.")
     while elegir==1:
         numjug=numjug+random.randint(1, 11)
         print(f"La suma de tus cartas es {numjug}")
         print("")
         if numjug > 21:
-            print(f"Tienes un {numjug}, te has pasado de 21. Has perdido. (-2 dinero)")
-            print("")
+            print(f"Tienes un {numjug}, te has pasado de 21. Has perdido. (-{apuesta} monedas)")
             elegir=0
             juego=0
-        elif input("¿Quieres robar? S/N: ") == "N":
+        elif input("¿Quieres robar? S/N: ").upper() == "N":
             print("")
             print(f"Te has plantado con un {numjug}")
             print("")
@@ -45,45 +64,53 @@ while juego==1:
     while elegcrup == 1:
         sleep(1.5)
         numcrupier=numcrupier+random.randint(1,11)
-        if numcrupier > 17 and numcrupier < 22:
-            print(f"El crupier se ha plantado con un {numcrupier}")
+        if numcrupier > 17 and numcrupier < 22 or numcrupier > numjug:
+            print(f"El crupier ha robado y se ha plantado con un {numcrupier}")
             print("")
             elegcrup=0
             if numcrupier < numjug:
                 print(f"Has sacado un {numjug} y el crupier un {numcrupier}")
-                print("Has ganado. (+4 monedas)")
+                print(f"Has ganado. (+{apuesta*2} monedas)")
                 print("")
                 dinero=dinero+4
+            elif numcrupier == numjug:
+                print(f"Has sacado un {numjug} y el crupier un {numcrupier}")
+                print("Habeis empatado. Se te devuelven las monedas.)")
+                print("")
+                dinero=dinero+2
             else:
                 print(f"Has sacado un {numjug} y el crupier un {numcrupier}")
-                print("Has perdido. (-2 monedas)")
+                print(f"Has perdido. (-{apuesta} monedas)")
                 print("")
         elif numcrupier > 21:
-            print(f"El crupier tiene un {numcrupier}, se ha pasado de 21. Has ganado. (+4 monedas)")
+            print(f"El crupier tiene un {numcrupier}, se ha pasado de 21. Has ganado. (+{apuesta*2} monedas)")
             elegcrup=0
-            dinero=dinero+4
+            dinero=dinero+apuesta*2
         else:
             print(f"El crupier ha robado y tiene un {numcrupier}")
             print("")
 
-if dinero > 0:
-    print("")
-    opcion=input("¿Quieres jugar otra ronda? S/N: ")
-    print("")
-else:
-    print("Te has quedado sin dinero. Fin de la partida.")
-    juego=0
-if opcion.upper() == "S":
-    print("Iniciando siguiente partida...")
-    print("")
-    sleep(2)
-    juego=1
-elif opcion.upper() == "N":
-    print(f"Te has retirado con {dinero} monedas. Fin del juego.")
-    juego=0
-else:
-    print(f"La opción {opcion} no es válida. Fin del juego.")
-    juego=0
+    if dinero > 0:
+        print("")
+        opcion=input("¿Quieres jugar otra ronda? S/N: ")
+        print("")
+        if opcion.upper() == "S":
+            print("Iniciando siguiente partida...")
+            print("")
+            sleep(2)
+            juego=1
+        elif opcion.upper() == "N":
+            print(f"Te has retirado con {dinero} monedas. Fin del juego.")
+            print("")
+            juego=0
+        else:
+            print(f"La opción {opcion} no es válida. Fin del juego.")
+            print("")
+            juego=0
+    else:
+        print("Te has quedado sin dinero. Fin de la partida.")
+        print("")
+        juego=0
 
 
 
