2e2b992729573ed823ce7d6abcc88da7383d05d3
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-11-07 16:05:43 +0100

    Update Ejercicio41.py

--- EJERCICIOS BUCLES/Ejercicio41.py
+++ EJERCICIOS BUCLES/Ejercicio41.py
@@ -1,9 +1,9 @@
 # 41: Imprime el patrón utilizando "for".
 
-indice=1
+indice=5
 num=0
 
 for i in range(0,5):
     num=str(num)+str(indice)
-    indice=indice+1
+    indice=indice-1
     print(str(num))
