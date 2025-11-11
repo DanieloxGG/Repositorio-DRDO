0421393a35da71bc3a32b77ec42dd17b172660a1
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-11-11 10:45:08 +0100

    Update Ejercicio46.py

---
+++ EJERCICIOS BUCLES/Ejercicio46.py
@@ -0,0 +1,9 @@
+# 46: A partir del programa anterior, soluciona el error que se produce en el test anterior con la 
+# palabra Abrigo utilizando únicamente una instrucción.
+
+# Tal y como lo he hecho yo ya funciona, pero podriamos poner un...
+
+palabra="Hola"
+palabra=palabra.lower
+
+# ... antes del bucle para transformarlo todo en minúsculas.
