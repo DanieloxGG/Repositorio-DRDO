f2763eace469c7770731b83c822cfbb50c35c2ef
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-11-21 16:04:36 +0100

    Create Ejercicio57.py

---
+++ EJERCICIOS BUCLES WHILE/Ejercicio57.py
@@ -0,0 +1,4 @@
+# 57: Realiza un programa que permita adivinar un número comprendido entre 1 y 5. El programa 
+# controlar si el usuario introduce un número no comprendido entre 1 y 5
+
+import random
