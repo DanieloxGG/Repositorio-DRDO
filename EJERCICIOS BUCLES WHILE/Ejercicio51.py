9717bd13bf93fc71ce55331dffa895e265b439f0
DanieloxGG <daniel.rodriguez2@campus.xaloc.org>
2025-11-21 16:04:18 +0100

    Create Ejercicio51.py

---
+++ EJERCICIOS BUCLES WHILE/Ejercicio51.py
@@ -0,0 +1,9 @@
+# 51: A partir del programa anterior, modifica el código para que sea el usuario quién introduzca el 
+# número de veces que desea que repita la frase Buenos días. Con While
+
+i=0
+n=float(input("Introduce el número de veces: "))
+
+while i < n:
+    print("Buenos días.")
+    i=i+1
